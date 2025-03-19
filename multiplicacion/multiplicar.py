2daaf8dba664730f7b505debd84c445b64399a72
elCheetah <eabraham.pro@gmail.com>
2025-03-18 22:32:49 -0400

    operacion multiplicacion

---
+++ multiplicacion/multiplicar.py
@@ -0,0 +1,9 @@
+# Solicitar los dos números al usuario
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+# Calcular la multiplicación
+resultado = num1 * num2
+
+# Mostrar el resultado
+print(f"El resultado de la multiplicación es: {resultado}")
